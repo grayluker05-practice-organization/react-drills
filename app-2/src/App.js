52a27783399dd07a111270834523c88c6951319c
grayluker05 <graysonluker@gmail.com>
2019-11-30 11:36:20 -0700

    app 2 done twice

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      friends: ['Josh', 'Reese', 'Maxwell', 'Christian']
+    }
+  }
+
   render() {
+    let friendsDisplay = this.state.friends.map((e, i) => {
+      return <h2 key={i}>{e}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {friendsDisplay}
       </div>
     );
   }
